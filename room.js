01a0c46e3242eb508d83b8d0f41af5910ac07d99
Aishika23 <100014326+Aishika23@users.noreply.github.com>
2022-06-13 17:02:15 +0530

    Add files via upload

---
+++ room.js
@@ -0,0 +1,10 @@
+firebaseConfig = {
+    apiKey: "AIzaSyBSJzQCruNZ4Gq6qzwK-eQ4ySMhlfF6NSY",
+    authDomain: "letschat-b1a2b.firebaseapp.com",
+    databaseURL: "https://letschat-b1a2b-default-rtdb.firebaseio.com",
+    projectId: "letschat-b1a2b",
+    storageBucket: "letschat-b1a2b.appspot.com",
+    messagingSenderId: "475055371294",
+    appId: "1:475055371294:web:41b30f9c5ea4998f18ea0f",
+    measurementId: "G-N29Q7QE57C"
+  };
